ea6eff1ff365470366b73b6026cadb75d732b1a3
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2025-04-01 12:56:30 +0530

    Initial routes Commit

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import { AdminRoutes, VendorRoutes } from "./routes/index";
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.json("hello this is the food a order app");
-  return;
-});
+app.use("/admin", AdminRoutes);
+app.use("/vendor", VendorRoutes);
 
 app.listen(8000, () => {
   console.log("App is Listening to Port 8000");
--- routes/AdminRoutes.ts
+++ routes/AdminRoutes.ts
@@ -1,4 +1,8 @@
 import express, { Request, Response, NextFunction } from "express";
 const router = express.Router();
 
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.json({ message: "Hello from the Admin" });
+});
+
 export { router as AdminRoutes };
