11232b8eb0e13d1ef8d82e05cc5018273dd662bc
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2025-04-01 16:00:04 +0530

    Added functionality for creating vendor

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,11 +1,54 @@
 import { Request, Response, NextFunction } from "express";
 
+import { CreateVendorInput } from "../dto";
+import { Vendor } from "../models";
+import { GeneratePassword, GenerateSalt } from "../utility";
+
 export const CreateVendor = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.json({ message: "Vendor Created Successfully" });
+  const {
+    address,
+    email,
+    foodType,
+    name,
+    ownerName,
+    password,
+    phone,
+    pincode,
+  } = <CreateVendorInput>req.body;
+
+  const existingVendor = await Vendor.findOne({ email });
+
+  if (existingVendor) {
+    res.status(409).json({ message: "A Vendor exists with this emailId" });
+    return;
+  }
+
+  //Generate Salt
+  const salt = await GenerateSalt();
+
+  //Encrypt the Password using the Salt
+  const HashedPassword = await GeneratePassword(password, salt);
+
+  const CreateVendor = await Vendor.create({
+    name,
+    address,
+    email,
+    foodType,
+    ownerName,
+    password: HashedPassword,
+    phone,
+    pincode,
+    salt,
+    serviceAvailability: false,
+    coverImages: [],
+    rating: 0,
+  });
+
+  res.status(200).json(CreateVendor);
 };
 
 export const GetVendors = async (
--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
 import { AdminRoutes, VendorRoutes } from "./routes/index";
+import { MONGO_URI } from "./config";
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/admin", AdminRoutes);
 app.use("/vendor", VendorRoutes);
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.listen(8000, () => {
   console.log("App is Listening to Port 8000");
